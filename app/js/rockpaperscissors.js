ecea2d76d43d3d17caae026c1b8f185719f238c9
savageblackout <clairejsavage@gmail.com>
2016-01-12 14:47:09 -0800

    I made changes for Unit 4 Homework git push origin master

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,26 +22,34 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+
+return move || getInput();
+
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    
+    return move || randomPlay();
 }
 
-function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+ffunction getWinner(playerMove,computerMove) {
+   
+    if(playerMove === computerMove){
+        return "tie";
+        }
+        else if(playerMove === "rock"&&computerMove === "scissors"){
+            return "player wins";
+        }else if(playerMove ==="rock"&&computerMove === "paper"){
+            return "computer wins";
+        }else if(playerMove ==="paper"&&computerMove === "scissors"){
+            return "computer wins"
+        }else if(playerMove === "paper"&&computerMove === "rock"){
+            return "player wins"
+        }else if(plaerMove === "rock"&&computerMove === "paper"){
+            return "computer wins"
+        }else if(playerMove ==="rock"&&computerMove ==="scissors"){
+            return "player wins"
+        }
 }
 
 function playToFive() {
